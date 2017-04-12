2608dea7e05a206fad74c52cfeee322505b05f65
Christoffel Janssens <janssens.christoffel@gmail.com>
2017-04-12 14:52:00 +0200

    Update WaterMeterPulseSensor2.ino

--- examples/WaterMeterPulseSensor/WaterMeterPulseSensor2.ino
+++ examples/WaterMeterPulseSensor/WaterMeterPulseSensor2.ino
@@ -1,39 +1,3 @@
-/**
- * The MySensors Arduino library handles the wireless radio link and protocol
- * between your home built sensors/actuators and HA controller of choice.
- * The sensors forms a self healing radio network with optional repeaters. Each
- * repeater and gateway builds a routing tables in EEPROM which keeps track of the
- * network topology allowing messages to be routed to nodes.
- *
- * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2015 Sensnology AB
- * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
- *
- * Documentation: http://www.mysensors.org
- * Support Forum: http://forum.mysensors.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- *******************************
- *
- * REVISION HISTORY
- * Version 1.0 - Henrik Ekblad
- * Version 1.1 - GizMoCuz
- *
- * DESCRIPTION
- * Use this sensor to measure volume and flow of your house watermeter.
- * You need to set the correct pulsefactor of your meter (pulses per m3).
- * The sensor starts by fetching current volume reading from gateway (VAR 1).
- * Reports both volume and flow back to gateway.
- *
- * Unfortunately millis() won't increment when the Arduino is in
- * sleepmode. So we cannot make this sensor sleep if we also want
- * to calculate/report flow.
- * http://www.mysensors.org/build/pulse_water
- */
-
 // Enable debug prints to serial monitor
 #define MY_DEBUG
 
@@ -47,108 +11,101 @@
 
 #define PULSE_FACTOR 1000                       // Nummber of blinks per m3 of your meter (One rotation/liter)
 
-#define SLEEP_MODE false                        // flowvalue can only be reported when sleep mode is false.
-
-#define MAX_FLOW 40                             // Max flow (l/min) value to report. This filters outliers.
-
 #define CHILD_ID 1                              // Id of the sensor child
 
 unsigned long SEND_FREQUENCY =
-    5000;           // Minimum time between send (in milliseconds). We don't want to spam the gateway (30000 ms = 30s).
+  5000;           // Minimum time between send (in milliseconds). We don't want to spam the gateway (30000 ms = 30s).
 
-MyMessage flowMsg(CHILD_ID,V_FLOW);
-MyMessage volumeMsg(CHILD_ID,V_VOLUME);
-MyMessage lastCounterMsg(CHILD_ID,V_VAR1);
+MyMessage volumeMsg(CHILD_ID, V_VOLUME);
+MyMessage lastCounterMsg(CHILD_ID, V_VAR1);
 
-double ppl = ((double)PULSE_FACTOR)/1000;        // Pulses per liter
+double ppl = ((double)PULSE_FACTOR) / 1000;      // Pulses per liter
 
 volatile unsigned long pulseCount = 0;
 volatile unsigned long lastBlink = 0;
-volatile double flow = 0;
 bool pcReceived = false;
 unsigned long oldPulseCount = 0;
 unsigned long newBlink = 0;
-double oldflow = 0;
-double volume =0;
-double oldvolume =0;
-unsigned long lastSend =0;
-unsigned long lastPulse =0;
+double volume = 0;
+double oldvolume = 0;
+unsigned long lastSend = 0;
+unsigned long lastPulse = 0;
 
 void setup()
 {
-	// initialize our digital pins internal pullup resistor so one pulse switches from high to low (less distortion)
-	pinMode(DIGITAL_INPUT_SENSOR, INPUT_PULLUP);
+  // initialize our digital pins internal pullup resistor so one pulse switches from high to low (less distortion)
+  pinMode(DIGITAL_INPUT_SENSOR, INPUT_PULLUP);
 
-	pulseCount = oldPulseCount = 0;
+  pulseCount = oldPulseCount = 0;
 
-	// Fetch last known pulse count value from gw
-	request(CHILD_ID, V_VAR1);
+  // Fetch last known pulse count value from gw
+  request(CHILD_ID, V_VAR1);
 
-	lastSend = lastPulse = millis();
+  lastSend = lastPulse = millis();
 
-	attachInterrupt(digitalPinToInterrupt(DIGITAL_INPUT_SENSOR), onPulse, RISING);
+  attachInterrupt(digitalPinToInterrupt(DIGITAL_INPUT_SENSOR), onPulse, RISING);
 }
 
 void presentation()
 {
-	// Send the sketch version information to the gateway and Controller
-	sendSketchInfo("Water Meter", "1.1");
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo("Water Meter", "1.1");
 
-	// Register this device as Waterflow sensor
-	present(CHILD_ID, S_WATER);
+  // Register this device as Watersensor
+  present(CHILD_ID, S_WATER);
 }
 
 void loop()
 {
-	unsigned long currentTime = millis();
+  unsigned long currentTime = millis();
 
-	// Only send values at a maximum frequency or woken up from sleep
-	if (currentTime - lastSend > SEND_FREQUENCY) {
-		lastSend=currentTime;
+  // Only send values at a maximum frequency or woken up from sleep
+  if (currentTime - lastSend > SEND_FREQUENCY) {
+    lastSend = currentTime;
 
-		if (!pcReceived) {
-			//Last Pulsecount not yet received from controller, request it again
-			request(CHILD_ID, V_VAR1);
-			return;
-		}
+    if (!pcReceived) {
+      //Last Pulsecount not yet received from controller, request it again
+      request(CHILD_ID, V_VAR1);
+      return;
+    }
 
-		// Pulse count has changed
-		if (pulseCount != oldPulseCount) {
-			oldPulseCount = pulseCount;
+    // Pulse count has changed
+    if (pulseCount != oldPulseCount) {
+      oldPulseCount = pulseCount;
 
-			Serial.print("pulsecount:");
-			Serial.println(pulseCount);
+      Serial.print("pulsecount:");
+      Serial.println(pulseCount);
 
-			send(lastCounterMsg.set(pulseCount));                  // Send  pulsecount value to gw in VAR1
+      send(lastCounterMsg.set(pulseCount));                  // Send  pulsecount value to gw in VAR1
 
-			double volume = ((double)pulseCount/((double)PULSE_FACTOR));
-			if (volume != oldvolume) {
-				oldvolume = volume;
+      double volume = ((double)pulseCount / ((double)PULSE_FACTOR));
+      if (volume != oldvolume) {
+        oldvolume = volume;
 
-				Serial.print("volume:");
-				Serial.println(volume, 3);
+        Serial.print("volume:");
+        Serial.println(volume, 3);
 
-				send(volumeMsg.set(volume, 3));               // Send volume value to gw
-			}
-		}
-	}
+        send(volumeMsg.set(volume, 3));               // Send volume value to gw
+      }
+    }
+  }
 }
 
 void receive(const MyMessage &message) //Haal 1 keer de waarden op van gateway
 {
-	if (message.type==V_VAR1) {
-		unsigned long gwPulseCount=message.getULong();
-		pulseCount += gwPulseCount; //pulseCount = 162516
-		Serial.print("Received last pulse count from gw:");
-		Serial.println(pulseCount);
-		pcReceived = true;
-	}
+  if (message.type == V_VAR1) {
+    unsigned long gwPulseCount = message.getULong();
+    pulseCount += gwPulseCount; //pulseCount = 162516
+    Serial.print("Received last pulse count from gw:");
+    Serial.println(pulseCount);
+    pcReceived = true;
+  }
 }
 
 void onPulse()
 {
-	Serial.print("PulsGezien");
-	pulseCount++;
-	Serial.print("Nieuwe pulsecount:");
-	Serial.println(pulseCount);
+  Serial.print("PulsGezien");
+  pulseCount++;
+  Serial.print("Nieuwe pulsecount:");
+  Serial.println(pulseCount);
 }
